c149932407f475170a71b2e4bfb4e9f8c88095a2
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-10-30 12:33:47 +0300

    🍵🍵🍵post requests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 const app = express();
+//enabling app to accept json input
+app.use(express.json());
 //courses object
 
 const courses = [
@@ -17,23 +19,32 @@ app.get("/", (req, res) => {
 //courses endpoint
 app.get("/api/courses", (req, res) => {
   res.send(courses);
+});
+//courses/id endpoint
+app.get("/api/courses/:id", (req, res) => {
+  //let is used to name a variable that you intend to reset later
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send(`Course with id not found`);
+  res.send(course);
+});
+// using more than one parameters
+app.get("/api/courses/:id/:name", (req, res) => {
+  res.send(req.params);
+});
+// using query parameters
+app.get("/api/courses/:id/:name", (req, res) => {
+  res.send(req.query);
+});
+// post endpoint
 
-  //courses/id endpoint
-  app.get("/api/courses/:id", (req, res) => {
-    let course = courses.find((c) => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send(`Course with id not found`);
-    res.send(course)
-  });
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
 
-  // using more than one parameters
-  app.get("/api/courses/:id/:name", (req, res) => {
-    res.send(req.params);
-  });
-
-  // using query parameters
-  app.get("/api/courses/:id/:name", (req, res) => {
-    res.send(req.query);
-  });
+  courses.push(course);
+  res.send(course);
 });
 
 //PORT
