f742766624772249ddd572e1e348ef61d967ab6c
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-10-30 12:03:19 +0300

    🍵🍵🍵handling get requests

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 import express from "express";
 
-const app = express()
+const app = express();
+//courses object
 
-app.get('/',(req,res)=>{
-    
-})
+const courses = [
+  { id: 1, name: "CS" },
+  { id: 2, name: "IT" },
+  { id: 3, name: "DSA" },
+];
+
+//home endpoinyt
+app.get("/", (req, res) => {
+  res.send(`Hello Express`);
+});
+
+//courses endpoint
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+
+  //courses/id endpoint
+  app.get("/api/courses/:id", (req, res) => {
+    let course = courses.find((c) => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send(`Course with id not found`);
+    res.send(course)
+  });
+
+  // using more than one parameters
+  app.get("/api/courses/:id/:name", (req, res) => {
+    res.send(req.params);
+  });
+
+  // using query parameters
+  app.get("/api/courses/:id/:name", (req, res) => {
+    res.send(req.query);
+  });
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port ${port}...`));
